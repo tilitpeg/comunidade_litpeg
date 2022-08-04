79851ad75c0f4578b5379bfd6a220d691414f884
Thiago <thiagorb2000@gmail.com>
2022-08-03 22:59:23 -0300

    recolocando secret_key

--- project/settings.py
+++ project/settings.py
@@ -14,10 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@sbt9p!0_wvw@bv0%a@$94xfc3evy=*4h8k-l@liic5tj+5b3z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+# DEBUG = os.getenv('DEBUG', False)
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
