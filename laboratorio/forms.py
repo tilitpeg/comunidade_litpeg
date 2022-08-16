72b0a0c5981d5316b373282160d99a49de443afb
Thiago <thiagorb2000@gmail.com>
2022-08-15 21:13:31 -0300

    atualiza classe Pessoa e adiciona campo ao formulário PessoaForm

--- laboratorio/forms.py
+++ laboratorio/forms.py
@@ -10,4 +10,4 @@ class LaboratorioForm(forms.ModelForm):
 class PessoaForm(forms.ModelForm):
     class Meta:
         model = Pessoa
-        fields = ['nome_completo', 'email', 'numero_cracha']
+        fields = ['nome_completo', 'email', 'numero_cracha','funcao']
--- laboratorio/models.py
+++ laboratorio/models.py
@@ -12,10 +12,16 @@ class Laboratorio(models.Model):
 
 
 class Pessoa(models.Model):
+  FUNCOES = [
+    ("Docente", "Docente"),
+    ("Discente","Discente"),
+    ("Técnico", "Técnico"),
+  ]
+
   nome_completo = models.CharField(max_length=256, verbose_name='Nome')
   email = models.EmailField(verbose_name='E-mail')
   numero_cracha = models.CharField(max_length=25, verbose_name='Número do Crachá')
-  funcao = models.CharField(max_length=25, default='', verbose_name='Função')
+  funcao = models.CharField(max_length=25, choices=FUNCOES, default='None', verbose_name='Função')
   laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
 
   def __str__(self) -> str:
