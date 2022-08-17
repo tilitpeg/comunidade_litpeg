58be7e25eeb314a0734f893e579389051c48cc61
Thiago <thiagorb2000@gmail.com>
2022-08-17 15:25:42 -0300

    Estruturando o Django Admin

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import Laboratorio, Pessoa
 
-# Register your models here.
-admin.site.register(Laboratorio)
-admin.site.register(Pessoa)
+
+class LaboratorioAdmin(admin.ModelAdmin):
+  list_display = ('nome_lab', 'usuario')
+  search_fields = ('nome_lab', 'usuario')
+
+
+class PessoaAdmin(admin.ModelAdmin):
+  list_display = ('nome_completo', 'email', 'numero_cracha', 'funcao', 'laboratorio')
+  search_fields = ('nome_completo', 'email', 'numero_cracha', 'funcao', 'laboratorio')
+  list_filter = ('laboratorio',)
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+admin.site.register(Pessoa, PessoaAdmin)
--- project/urls.py
+++ project/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('main.urls')),
     path('laboratorio/', include('laboratorio.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
