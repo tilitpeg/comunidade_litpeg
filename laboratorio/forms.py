1c43ea8bf4d6b1abc89f05f93c78c47e56a0b186
Thiago <thiagorb2000@gmail.com>
2022-08-18 00:32:37 -0300

    Validação de Usuário e Email repetido

--- laboratorio/forms.py
+++ laboratorio/forms.py
@@ -1,5 +1,8 @@
+import imp
 from django import forms
 from .models import Laboratorio, Pessoa
+from django.core.exceptions import ValidationError
+
 
 class LaboratorioForm(forms.ModelForm):
   class Meta:
@@ -11,3 +14,17 @@ class PessoaForm(forms.ModelForm):
     class Meta:
         model = Pessoa
         fields = ['nome_completo', 'email', 'numero_cracha','funcao']
+
+    def clean_nome_completo(self):
+      nome = self.cleaned_data['nome_completo']
+      if Pessoa.objects.filter(nome_completo=nome).exists():
+        raise ValidationError(f'Erro! O nome {nome} já está cadastrado.')
+      
+      return nome
+
+    def clean_email(self):
+      email_field = self.cleaned_data['email']
+      if Pessoa.objects.filter(email=email_field).exists():
+        raise ValidationError(f'Erro! O email {email_field} já está cadastrado.')
+      
+      return email_field
